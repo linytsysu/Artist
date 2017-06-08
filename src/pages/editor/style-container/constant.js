e1623be0e72f37756bf0f0e6f8cb66be4ed69924
linytsysu <linytsysu@163.com>
2017-06-08 23:04:26 +0800

    Add all the styles

--- src/pages/editor/style-container/constant.js
+++ src/pages/editor/style-container/constant.js
@@ -1,5 +1,9 @@
 export const styleNames = [
-  'style0', 'style1', 'style2', 'style3', 'style4', 'style5',
+  'style0', 'style1', 'style2', 'style3', 'style4',
+  'style5', 'style6', 'style7', 'style8', 'style9',
+  'style10', 'style11', 'style12', 'style13', 'style14',
+  'style15', 'style16', 'style17', 'style18', 'style19',
+  'style20', 'style21', 'style22', 'style23', 'style24', 'style25'
 ];
 
 export function getImage(name) {
@@ -16,5 +20,45 @@ export function getImage(name) {
     return require('../../../assets/thumbnails/style4.jpg');
   case 'style5':
     return require('../../../assets/thumbnails/style5.jpg');
+  case 'style6':
+    return require('../../../assets/thumbnails/style6.jpg');
+  case 'style7':
+    return require('../../../assets/thumbnails/style7.jpg');
+  case 'style8':
+    return require('../../../assets/thumbnails/style8.jpg');
+  case 'style9':
+    return require('../../../assets/thumbnails/style9.jpg');
+  case 'style10':
+    return require('../../../assets/thumbnails/style10.jpg');
+  case 'style11':
+    return require('../../../assets/thumbnails/style11.jpg');
+  case 'style12':
+    return require('../../../assets/thumbnails/style12.jpg');
+  case 'style13':
+    return require('../../../assets/thumbnails/style13.jpg');
+  case 'style14':
+    return require('../../../assets/thumbnails/style14.jpg');
+  case 'style15':
+    return require('../../../assets/thumbnails/style15.jpg');
+  case 'style16':
+    return require('../../../assets/thumbnails/style16.jpg');
+  case 'style17':
+    return require('../../../assets/thumbnails/style17.jpg');
+  case 'style18':
+    return require('../../../assets/thumbnails/style18.jpg');
+  case 'style19':
+    return require('../../../assets/thumbnails/style19.jpg');
+  case 'style20':
+    return require('../../../assets/thumbnails/style20.jpg');
+  case 'style21':
+    return require('../../../assets/thumbnails/style21.jpg');
+  case 'style22':
+    return require('../../../assets/thumbnails/style22.jpg');
+  case 'style23':
+    return require('../../../assets/thumbnails/style23.jpg');
+  case 'style24':
+    return require('../../../assets/thumbnails/style24.jpg');
+  case 'style25':
+    return require('../../../assets/thumbnails/style25.jpg');
   }
 }
